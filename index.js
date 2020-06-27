33740494a6e2f93b1c32840492e5ec56240128dc
rhld16 <24809571+rhld16@users.noreply.github.com>
2020-06-27 18:15:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ client.on("guildMemberRemove", member => { stats.left(member) });
 client.on("guildMemberAdd", member => { stats.new(member) });
 client.on("messageReactionAdd", async (reaction, user) => { reactions.add(reaction, user) });
 client.on("messageReactionRemove", async (reaction, user) => { reactions.remove(reaction, user) });
+music();
 const http = require("http"), express = require("express"), app = express();
 app.get("/", (request, response) => {response.sendStatus(200)});
 app.listen(process.env.PORT);
