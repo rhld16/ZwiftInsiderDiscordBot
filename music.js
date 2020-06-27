eaf995f3f66086a763320c0292222fe8ff5ca20e
Rhodri <24809571+rhld16@users.noreply.github.com>
2020-06-27 18:08:28 +0100

    Create music.js

---
+++ music.js
@@ -0,0 +1,64 @@
+const Discord = require("discord.js"),
+  ytdl = require("ytdl-core"),
+  client = new Discord.Client(),
+  chanid = '718449584371662880'
+module.exports = {
+  leave(musich);
+  console.log("bot ready");
+  play(musich);
+}),
+  client.on("message", e => {
+    if (e.content.startsWith(".")) {
+      let n = e.content.substring(1).split(" ")[0],
+      o = commands[n];
+      o.process(e);
+    }
+  });
+const commands = {
+  ping: {
+    process: function(e) {
+      e.channel.send({
+        embed: { description: "🏓 pong! my ping is " + client.ws.ping + "ms" }
+      });
+    }
+  },
+  join: {
+    process: function(e) {
+      play();
+    }
+  },
+  leave: {
+    process: function(e) {
+      leave();
+    }
+  },
+  reboot: {
+    process: function(e) {
+      leave(),
+      console.log("rebooting"), 
+      process.exit(0);
+    }
+  }
+};
+function play() {
+  var musich = client.channels.cache.get(chanid);
+  musich.join().then(n => {
+    console.log("joined"),
+      n
+        .play(ytdl("https://www.youtube.com/watch?v=36YnV9STBqc"), {
+          filter: "audioonly"
+        })
+        .on("finish", () => {
+          leave(), play(), console.log("repeat");
+        });
+  });
+}
+function leave() {
+  var musich = client.channels.cache.get(chanid);
+  console.log("leaving");
+  musich.leave();
+}
+  setInterval(function() {
+    process.exit(0);
+  }, 21600000);
+}
