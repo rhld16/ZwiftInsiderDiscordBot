a717e5644927142323abe4d5f22ea402f9d25c9e
rhld16 <24809571+rhld16@users.noreply.github.com>
2020-06-28 10:34:43 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,3 @@
-const prefix = "!"
 module.exports = {
 	name: 'help',
 	description: 'List all of my commands or info about a specific command.',
@@ -7,29 +6,11 @@ module.exports = {
 	execute(message, args) {
 		const data = [];
 		const { commands } = message.client;
-
-		if (!args.length) {
 			data.push('Here\'s a list of all my commands:');
-			data.push(commands.map(command => command.name).join(', '));
-			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+			data.push('8ball, ban, flip, help, hi, ping, purge, say, shrug, stats, time');
+			data.push('This bot also offers 24/7 music in the 24/7 music voice channel');
       message.author.send(data, { split: true })
      if(message.channel.type==='text'){
       return message.reply('I\'ve sent you a DM with all my commands!')};
-		} else {
-
-		const name = args[0].toLowerCase();
-		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
-
-		if (!command) {
-			return message.reply('that\'s not a valid command!');
-		}
-
-		data.push(`**Name:** ${command.name}`);
-
-		if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-		if (command.description) data.push(`**Description:** ${command.description}`);
-		if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
-
-		message.channel.send(data, { split: true });
-	}}
+	}
 };
