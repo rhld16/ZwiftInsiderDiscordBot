b75979da77ab9c086e54470f04d56f33d2f8464e
rhld16 <24809571+rhld16@users.noreply.github.com>
2020-06-28 10:21:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
-const Discord = require("discord.js");
-const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
-const reactions = require("./reactions.js");
-const stats = require("./stats.js");
-const handler = require("./handler.js");
-const music = require("./music.js");
+const Discord = require("discord.js"),
+client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]}),
+reactions = require("./reactions.js"),
+stats = require("./stats.js"),
+handler = require("./handler.js"),
+ytdl = require("ytdl-core"),
+client = new Discord.Client(),
+chan = '718449584371662880';
+function play() {
+  musich.join().then(n => {
+    console.log("joined"),
+      n.play(ytdl("https://www.youtube.com/watch?v=36YnV9STBqc"), {filter: "audioonly"})
+        .on("finish", () => {
+          leave(), play(), console.log("repeat");
+        });
+  });
+}
+function leave() {
+  var musich = client.channels.cache.get(chan);
+  console.log("leaving");
+  musich.leave();
+}
 
 client.once("ready", () => {
   client.user.setPresence({ activity: { type: "PLAYING", name: "Zwift | use !help" }});
+  const musich = client.channels.cache.get(chan);
+  play(musich);
   console.log("Ready!")});
 client.on("message", message => { handler(message) });
 client.on("guildMemberRemove", member => { stats.left(member) });
 client.on("guildMemberAdd", member => { stats.new(member) });
 client.on("messageReactionAdd", async (reaction, user) => { reactions.add(reaction, user) });
 client.on("messageReactionRemove", async (reaction, user) => { reactions.remove(reaction, user) });
-//music.player();
 const http = require("http"), express = require("express"), app = express();
 app.get("/", (request, response) => {response.sendStatus(200)});
 app.listen(process.env.PORT);
