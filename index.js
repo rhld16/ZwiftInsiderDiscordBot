60be6dced3ca58aa775ba00274a96ab7439979f6
rhld16 <24809571+rhld16@users.noreply.github.com>
2020-06-30 09:33:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ function play() {
       n.play(ytdl("https://www.youtube.com/watch?v=36YnV9STBqc"), {filter: "audioonly"})
         .on("finish", () => {
           leave(), play(), console.log("repeat");
-        })
-        .on("error", () => {
-          leave(), play(), console.log("fixed error ;)");
         });
   });
 }
