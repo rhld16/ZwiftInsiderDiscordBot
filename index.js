41fa1d629617c007df6914bb76b296bb45c88e0b
rhld16 <24809571+rhld16@users.noreply.github.com>
2021-01-17 13:32:24 +0000

    tab count

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js"),
 client = new Discord.Client();
+let tab = "650791518147313664"
 client.once("ready", () => {
   client.user.setPresence({ activity: { type: "PLAYING", name: "Zwift" }});
   console.log("Ready!")});
@@ -8,6 +9,15 @@ client.on("message", message => {
     message.crosspost().then(() => console.log('Crossposted message')).catch(console.error);
   }
 });
+client.on("guildMemberRemove", member => updateStats());
+client.on("guildMemberAdd", member => updateStats());
+function updateStats() {
+  try {
+    member.guild.channels.cache.get(tab).setName(`📊Total Zwifters: ${member.guild.memberCount}`);
+  } catch (e) {
+    console.log(e);
+  }
+}
 const http = require("http"), express = require("express"), app = express();
 app.get("/", (request, response) => {response.sendStatus(200)});
 app.listen(process.env.PORT);
