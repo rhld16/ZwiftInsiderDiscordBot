b68176ddbbe4e1a4f38c3bdf7b00395b331bf9b8
rhld16 <24809571+rhld16@users.noreply.github.com>
2021-01-17 21:51:49 +0000

    fix stat update

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const Discord = require("discord.js"),
 client = new Discord.Client();
-let tab = "650791518147313664"
+let tab = "650791518147313664";
 client.once("ready", () => {
   client.user.setPresence({ activity: { type: "PLAYING", name: "Zwift" }});
-  console.log("Ready!")});
-client.on("message", message => { 
-  if (message.channel.type === 'news') {
-    message.crosspost().then(() => console.log('Crossposted message')).catch(console.error);
-  }
+  console.log("Ready!")
 });
-client.on("guildMemberRemove", member => updateStats());
-client.on("guildMemberAdd", member => updateStats());
-function updateStats() {
+client.on("message", message => { 
+  if (message.channel.type === 'news') message.crosspost().then(() => console.log('Crossposted message')).catch(console.error);
+});
+client.on("guildMemberRemove", member => updateStats(member));
+client.on("guildMemberAdd", member => updateStats(member));
+function updateStats(member) {
   try {
     const format = num => String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, '$1,')
     member.guild.channels.cache.get(tab).setName(`📊Total Zwifters: ${format(member.guild.memberCount)}`);
