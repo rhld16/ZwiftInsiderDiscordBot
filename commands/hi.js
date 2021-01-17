ee68762545cff03a73c872889f5b54be50bab4ed
rhld16 <24809571+rhld16@users.noreply.github.com>
2021-01-17 00:17:02 +0000

    crossposter

--- commands/hi.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'hi',
-	description: 'Hi!',
-	execute(message) {
-		message.channel.send('Hello.');
-	},
-};
--- index.js
+++ index.js
@@ -1,37 +1,12 @@
 const Discord = require("discord.js"),
-client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]}),
-reactions = require("./reactions.js"),
-stats = require("./stats.js"),
-handler = require("./handler.js"),
-ytdl = require("ytdl-core-discord"),
-chan = '718449584371662880';
-async function play() {
-   const c = await client.channels.cache.get(chan).join();
-  console.log("joined");
-  const d = c.play(await ytdl("https://www.youtube.com/watch?v=36YnV9STBqc"), { type: 'opus' });
-  d.on('finish', () => {
-	console.log('finish!'),
-    c.disconnect();
-    play();
-  });
-}
+client = new Discord.Client();
 client.once("ready", () => {
-  client.user.setPresence({ activity: { type: "PLAYING", name: "Zwift | use !help" }});
+  client.user.setPresence({ activity: { type: "PLAYING", name: "Zwift" }});
   console.log("Ready!")});
-client.on("message", message => { handler(message) });
-client.on("guildMemberRemove", member => { stats.left(member) });
-client.on("guildMemberAdd", member => { stats.new(member) });
-client.on("messageReactionAdd", async (reaction, user) => { reactions.add(reaction, user) });
-client.on("messageReactionRemove", async (reaction, user) => { reactions.remove(reaction, user) });
-client.on('voiceStateUpdate', (oldMember, newMember) => {
-	console.log('change');
-  let newUserChannel = newMember.channel
-  let oldUserChannel = oldMember.channel
-  if (oldUserChannel === null) {
-		play();
-	} else if (newUserChannel === null && client.channels.cache.get(chan).members.size < 2) {
-		client.channels.cache.get(chan).leave();
-	}
+client.on("message", message => { 
+  if (message.channel.type === 'news') {
+    message.crosspost().then(() => console.log('Crossposted message')).catch(console.error);
+  }
 });
 const http = require("http"), express = require("express"), app = express();
 app.get("/", (request, response) => {response.sendStatus(200)});
