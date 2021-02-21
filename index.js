486fa1a69eec318bfa8a3d8d099823c5f21da085
rhld16 <24809571+rhld16@users.noreply.github.com>
2021-02-21 00:19:14 +0000

    tags start

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ client.once("ready", () => {
   client.user.setPresence({ activity: { type: "PLAYING", name: "Zwift" }});
   console.log("Ready!");
   let ziguild = client.guilds.cache.get('501890309039325224');
-  var interval = setInterval(() => updateStats(ziguild), 300000);
+  var interval = setInterval(() => updateStats(ziguild), 120000);
 });
 client.on("message", message => { 
+  let prefix = ".";
   if (message.channel.type === 'news') message.crosspost().then(() => console.log('Crossposted message')).catch(console.error);
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift();
+  // TAGS
+  if (command == "zwift")
+    sendTag(message.channel,
+      "Zwift",
+      "Zwift is a massively multiplayer online cycling and running physical training program that enables users to interact, train and compete in a virtual world.",
+      "https://yt3.ggpht.com/ytc/AAUvwnjfB9nd1KlJmdHPJaW0NugDoz0N95rPRDibiExf_Q=s900-c-k-c0x00ffffff-no-rj",
+      "https://www.zwift.com/");
 });
 client.on("guildMemberRemove", member => updateStats(member.guild));
 client.on('guildMemberAdd', member => {
@@ -22,6 +34,18 @@ function updateStats(guild) {
   } catch (e) {
     console.log(e);
   }
+}
+function sendTag(channel, title, body, img, link) {
+  const embed = {
+    "title": title,
+    "description": body,
+    "url": link,
+    "color": 3460666,
+    "thumbnail": {
+      "url": img
+    }
+  };
+  channel.send({ embed });
 }
 const http = require("http"), express = require("express"), app = express();
 app.get("/", (req, res) => {res.sendStatus(200)});
