fff843c6ae86734887793c6085a4366db9d5108f
rhld16 <24809571+rhld16@users.noreply.github.com>
2021-03-11 23:11:41 +0000

    more tags

--- index.js
+++ index.js
@@ -21,6 +21,24 @@ client.on("message", message => {
       "Zwift is a massively multiplayer online cycling and running physical training program that enables users to interact, train and compete in a virtual world.",
       "https://yt3.ggpht.com/ytc/AAUvwnjfB9nd1KlJmdHPJaW0NugDoz0N95rPRDibiExf_Q=s900-c-k-c0x00ffffff-no-rj",
       "https://www.zwift.com/");
+  if (command == "zi")
+    sendTag(message.channel,
+      "Zwift Insider",
+      "Zwift Insider is the community-driven source for all things Zwift. The team of contributors delivers an unmatched breadth and depth of content including Zwift news, hacks, training tips, and more.",
+      "https://pbs.twimg.com/profile_images/943153414292279297/Xm2M38U4_400x400.jpg",
+      "https://zwiftinsider.com/");
+  if (command == "discord")
+    sendTag(message.channel,
+      "Using Discord",
+      "Here is a simple guide on Zwift Insider on how to use Discord",
+      "http://webcamstartup.com/wp-content/uploads/2018/04/discord-logo.jpg",
+      "https://zwiftinsider.com/using-discord/");
+  if (command == "discordguide")
+    sendTag(messgae.channel,
+      "Discord Guide",
+      "Here is Discord's getting started guide",
+      "http://webcamstartup.com/wp-content/uploads/2018/04/discord-logo.jpg",
+      "https://support.discord.com/hc/en-us/articles/360045138571-Beginner-s-Guide-to-Discord#h_9de92bc2-3bca-459f-8efd-e1e2739ca4f4")
 });
 client.on("guildMemberRemove", member => updateStats(member.guild));
 client.on('guildMemberAdd', member => {
@@ -50,4 +68,4 @@ function sendTag(channel, title, body, img, link) {
 const http = require("http"), express = require("express"), app = express();
 app.get("/", (req, res) => {res.sendStatus(200)});
 app.listen(process.env.PORT);
-client.login(process.env.TOKEN);
+client.login("ODAwMTU3NDI4NzM3OTAwNTU0.YAOCrQ.FNMMGQWal6kmARvETKu8wZPF4ms");
